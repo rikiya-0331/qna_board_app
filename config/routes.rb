7c7a37806cb4c8340c6f1cdd5579b79116dff0cb
Rikiya Nakao <baseball.r.n@icloud.com>
2025-07-23 23:13:08 +0900

    Action Mailerの設定とメール送信機能の確認

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  if Rails.env.development?
+    mount LetterOpenerWeb::Engine, at: "/letter_opener"
+  end
+
   devise_for :users
   root 'questions#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
