37dcb7277904b83a12f0de75e9c1842e92eadfe8
Rikiya Nakao <baseball.r.n@icloud.com>
2025-07-23 23:58:22 +0900

    マイページ（ユーザーページ）の作成とクイズ履歴の表示

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,10 @@ Rails.application.routes.draw do
 
   devise_for :users
   root 'questions#index'
+
+  # マイページへのルーティング
+  resource :mypage, only: [:show]
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
