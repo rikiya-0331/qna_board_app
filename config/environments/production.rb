5fb5814d11ca6883d166f1dcd8618423e39e07a5
Rikiya Nakao <baseball.r.n@icloud.com>
2025-07-28 16:41:36 +0900

    fix

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -30,7 +30,7 @@ Rails.application.configure do
   # Disable serving static files from the 'public' folder by default since
   # Apache or NGINX already handles this.
   # RenderではWebサーバーがアセットを配信するため、falseにするかコメントアウト
-  config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
+  config.public_file_server.enabled = true
 
   # Compress JavaScripts and CSS.
   # config.assets.js_compressor = :uglifier # もし利用している場合
