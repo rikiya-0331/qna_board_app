7258f8da8122d25048db61a5333fde9b3f35ddbd
Rikiya Nakao <baseball.r.n@icloud.com>
2025-07-28 11:40:51 +0900

    質問詳細ページの実装

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -3,4 +3,11 @@ class QuestionsController < ApplicationController
     # includes(:questions) を使うことで、カテゴリとそれに関連する質問をまとめて取得
     @categories = Category.includes(:questions)
   end
+
+  def show
+    @question = Question.find(params[:id])
+    # @question に紐づく正解の answer_choice も取得しておくとビューで扱いやすい
+    # (schema.rb から questions テーブルに answer_jp/en があるため、直接それらを使用することも可能)
+    @correct_answer_choice = @question.answer_choices.find_by(is_correct: true)
+  end
 end
