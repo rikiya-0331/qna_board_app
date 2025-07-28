5d7c14718186ee373e50f95f792bc819ff8f074a
Rikiya Nakao <baseball.r.n@icloud.com>
2025-07-28 17:08:58 +0900

    本番環境でmailgunを使うためのSMTPのセッティング

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -73,6 +73,19 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
 
+  # ここにMailgunのSMTP設定を追加してください
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    :port           => ENV['MAILGUN_SMTP_PORT'],
+    :address        => ENV['MAILGUN_SMTP_SERVER'],
+    :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
+    :password       => ENV['MAILGUN_SMTP_PASSWORD'],
+    :domain         => ENV['MAILGUN_DOMAIN'],
+    :authentication => :plain,
+    :enable_starttls_auto => true
+  }
+
+
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
